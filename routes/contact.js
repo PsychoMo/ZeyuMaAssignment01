d90c21bb968b8159bd217ebe6c23b48beb61e677
PsychoMo <zymike09@hotmail.com>
2019-02-02 16:11:49 -0500

    display db in view

--- routes/contact.js
+++ routes/contact.js
@@ -7,18 +7,18 @@ let contact = require("../models/contact");
 
 /** Get contact list page - read operation */
 router.get("/", (req, res, next) => {
-  contact.find((err, contactList) => {
-    if (err) {
-      return console.error(err);
-    } else {
-      console.log(contactList);
+    contact.find((err, contactList) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log(contactList);
 
-      /** res.render('contacts/index', {
-            title: 'Contact List',
-            contactList: contactList
-       });*/
-    }
-  });
+            res.render('contacts/index', {
+                title: 'Contact List',
+                contactList: contactList
+            });
+        }
+    });
 });
 
 module.exports = router;
