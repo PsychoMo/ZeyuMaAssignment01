0dcb2206b1795eb50c83c50278493d69ed44b62c
PsychoMo <zymike09@hotmail.com>
2019-02-09 16:49:41 -0500

    created controller design pattern

--- routes/contact.js
+++ routes/contact.js
@@ -1,108 +1,26 @@
 let express = require("express");
 let router = express.Router();
-let mongoose = require("mongoose");
 
-// create a reference to the database schema
-let contactModel = require("../models/contact");
+let contactController = require('../controllers/contact');
 
 /** Get contact list page - read operation */
-router.get("/", (req, res, next) => {
-    contactModel.find((err, contactList) => {
-        if (err) {
-            return console.error(err);
-        } else {
-            console.log(contactList);
-
-            res.render('contacts/index', {
-                title: 'Contact List',
-                contactList: contactList
-            });
-        }
-    });
-});
+router.get("/", contactController.displayContactList);
 
 /** GET Route for the Add page
  * this will display the Add page
 */
 
-router.get('/add', (req, res, next) => {
-    res.render('contacts/add', {
-        title: 'Add New Contact'
-    })
-});
+router.get('/add', contactController.displayAddPage);
 
 /**POST Route for processing the Add */
-router.post('/add', (req, res, next) => {
-
-    let newContact = contactModel({
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "age": req.body.age
-    })
-
-    contactModel.create(newContact, (err, contactModel) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            // refresh the contact list
-            res.redirect('/contact-list');
-        }
-    })
-})
+router.post('/add', contactController.processAddPage);
 
 /** GET Request - display the Edit page */
-router.get('/edit/:id', (req, res, next) => {
+router.get('/edit/:id', contactController.displayEditPage);
 
-    let id = req.params.id;
-
-    contactModel.findById(id, (err, contactObject) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            // Show the edit view
-            res.render('contacts/edit', {
-                title: 'Edit Contact',
-                contact: contactObject
-            });
-        }
-    });
-})
-
-router.post('/edit/:id', (req, res, next) => {
-    let id = req.params.id;
-
-    let updatedContact = contactModel({
-        "_id": id,
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "age": req.body.age
-    });
-
-    contactModel.update({ _id: id }, updatedContact, (err) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.redirect('/contact-list');
-        }
-    })
-})
+router.post('/edit/:id', contactController.processEditPage);
 
 /** GET request to perform the delete action */
-router.get('/delete/:id', (req, res, next) => {
-    let id = req.params.id;
-
-    contactModel.remove({ _id: id }, (err) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            // refresh the contact list
-            res.redirect('/contact-list');
-        }
-    })
-})
+router.get('/delete/:id', contactController.performDelete);
 
 module.exports = router;
