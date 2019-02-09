a0ba9073f22cf06ec4d460fa1697b3e2d1e545d3
PsychoMo <zymike09@hotmail.com>
2019-02-09 14:52:12 -0500

    added Create functionality to the DB

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //"URI":"mongodb://localhost/test"
-    "URI":"mongodb://zeyumawk04:a123456@ds058508.mlab.com:58508/comp308-w2019-lessone4b"
+    "URI":"mongodb://localhost/test"
+    //"URI":"mongodb://zeyumawk04:a123456@ds058508.mlab.com:58508/comp308-w2019-lessone4b"
 }
